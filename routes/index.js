dae1ad19c4bc9bb30272c3c1660e8c6752710f64
SANKALP <sankalp.sharma156@gmail.com>
2017-11-06 18:55:41 -0800

    changes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
 
     router.post('/readNameAndRespond', function(req, res, next) {
         res.send('Hello' + req.body.name);
