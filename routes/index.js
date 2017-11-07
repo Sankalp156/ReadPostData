48a1b49a434675259d62fd36818f94dabb84c531
SANKALP <sankalp.sharma156@gmail.com>
2017-11-07 09:54:14 -0800

    changes 4

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 
-
+module.exports = router;
     router.post('/readNameAndRespond', function(req, res, next) {
         var value_name = req.body.name;
         res.send('Hello' + value_name);
 
 });
 
-module.exports = router;
+
